e1f1f166ab63f37d63092f755ba2f90a8188f0c2
Michael McDermott <michael.mcdermott@kitware.com>
2020-01-28 17:35:22 -0500

    more travis tinkering 2

--- tests/end2end.sh
+++ tests/end2end.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-[[ "travis" == $(echo "travis" | passcrux split --ratio 3/5 -e abc --stdin | tail -3 | passcrux combine -e abc --stdin) ]]
+[[ "travis" == $(echo "travis" | /tmp/passcrux split --ratio 3/5 -e abc --stdin | tail -3 | /tmp/passcrux combine -e abc --stdin) ]]
