7e592d906338b173d756b0f6f19092b8aaafe8e5
Michael McDermott <michael.mcdermott@kitware.com>
2020-01-28 17:43:26 -0500

    trying GOPATH/bin based path

--- tests/end2end.sh
+++ tests/end2end.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-[[ "travis" == $(echo "travis" | /tmp/passcrux split --ratio 3/5 -e abc --stdin | tail -3 | /tmp/passcrux combine -e abc --stdin) ]]
+[[ "travis" == $(echo "travis" | $GOPATH/bin/passcrux split --ratio 3/5 -e abc --stdin | tail -3 | $GOPATH/bin/passcrux combine -e abc --stdin) ]]
